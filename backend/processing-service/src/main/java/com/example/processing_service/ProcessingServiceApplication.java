0bc4a3f5f47b16943c14310a1260a8dfa06965a1
Kushagra Anit Singh <kushagraa.n@gmail.com>
2025-03-09 22:00:50 +0530

    mvn instead of gradle

--- backend/processing-service/src/main/java/com/example/processing_service/ProcessingServiceApplication.java
+++ backend/processing-service/src/main/java/com/example/processing_service/ProcessingServiceApplication.java
@@ -1,4 +1,4 @@
-package com.mpjmp.processing;
+package com.mpjmp.processing.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
