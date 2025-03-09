fbd2c5f5ccff01cd59bbbc6c1f0a760eb0b8bbeb
Kushagra Anit Singh <kushagraa.n@gmail.com>
2025-03-09 22:01:13 +0530

    build issue resolved

--- backend/storage-service/src/main/java/com/example/storage_service/StorageServiceApplication.java
+++ backend/storage-service/src/main/java/com/example/storage_service/StorageServiceApplication.java
@@ -1,4 +1,4 @@
-package com.mpjmp.storage;
+package com.mpjmp.storage.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/storage-service/src/test/java/com/example/storage_service/StorageServiceApplicationTests.java
+++ backend/storage-service/src/test/java/com/example/storage_service/StorageServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.storage_service;
+package com.mpjmp.storage.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
