7a64c407fb88f7757e6613e7863d9918e09c5a02
Kushagra Anit Singh <kushagraa.n@gmail.com>
2025-03-09 21:59:55 +0530

    pom changes for notifs

--- backend/notification-service/src/main/java/com/example/notification_service/NotificationServiceApplication.java
+++ backend/notification-service/src/main/java/com/example/notification_service/NotificationServiceApplication.java
@@ -1,4 +1,4 @@
-package com.mpjmp.notification;
+package com.mpjmp.notification.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/notification-service/src/test/java/com/example/notification_service/NotificationServiceApplicationTests.java
+++ backend/notification-service/src/test/java/com/example/notification_service/NotificationServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.notification_service;
+package com.mpjmp.notification.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
